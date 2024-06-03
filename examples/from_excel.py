6abad5894123bb9ed4e451944019b367e6be54f9
RemGab <71643572+RemGab@users.noreply.github.com>
2024-06-03 16:00:32 +0200

    a

--- examples/from_excel.py
+++ examples/from_excel.py
@@ -6,9 +6,9 @@ from pandasai import Agent
 
 # By default, unless you choose a different LLM, it will use BambooLLM.
 # You can get your free API key signing up at https://pandabi.ai (you can also configure it in your .env file)
-os.environ["PANDASAI_API_KEY"] = "your-api-key"
+os.environ["PANDASAI_API_KEY"] = "$2a$10$yv13u18eyh4o4pt32aB1puDceJI1kmVfI3vowSgq7uovkvPPkubRC"
 
-agent = Agent("examples/data/Loan payments data.xlsx")
-response = agent.chat("How many loans are from men and have been paid off?")
+agent = Agent("data/AMZN.csv")
+response = agent.chat("donne moi les derniers prix de amazon")
 print(response)
 # Output: 247 loans have been paid off by men.
